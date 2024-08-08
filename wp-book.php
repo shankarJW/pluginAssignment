80e87c6cb56f9ab1773edb5a58eb24e0ce702d55
shankar wangwad <shankar.wangwad@hbwsl.com>
2024-08-08 20:33:09 +0530

    step5: create custom meta table

--- wp-book.php
+++ wp-book.php
@@ -12,7 +12,21 @@ register_activation_hook(__FILE__, 'wp_book_activate');
 register_deactivation_hook(__FILE__, 'wp_book_deactivate');
 
 function wp_book_activate() {
-   
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'book_meta';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        book_id bigint(20) NOT NULL,
+        author_name varchar(255) DEFAULT '' NOT NULL,
+        price decimal(10,2) DEFAULT '0.00' NOT NULL,
+        PRIMARY KEY  (id),
+        KEY book_id (book_id)
+    ) $charset_collate;";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
 }
 
 function wp_book_deactivate() {
