87df08fc85e0fa6375436f6139ef3482042cbb67
shankar wangwad <shankar.wangwad@hbwsl.com>
2024-08-08 20:13:22 +0530

    step3: register custom taxonomies

--- includes/wp-book-functions.php
+++ includes/wp-book-functions.php
@@ -24,3 +24,44 @@ function wp_book_register_post_type() {
 add_action('init', 'wp_book_register_post_type');
 
 
+// Register Custom Taxonomies
+function wp_book_register_taxonomies() {
+    // Book Category (Hierarchical)
+    $args = array(
+        'labels' => array(
+            'name' => 'Book Categories',
+            'singular_name' => 'Book Category',
+            'search_items' => 'Search Categories',
+            'all_items' => 'All Categories',
+            'parent_item' => 'Parent Category',
+            'parent_item_colon' => 'Parent Category:',
+            'edit_item' => 'Edit Category',
+            'update_item' => 'Update Category',
+            'add_new_item' => 'Add New Category',
+            'new_item_name' => 'New Category Name',
+            'menu_name' => 'Categories',
+        ),
+        'hierarchical' => true,
+        'public' => true,
+    );
+    register_taxonomy('book_category', 'book', $args);
+
+    // Book Tag (Non-Hierarchical)
+    $args = array(
+        'labels' => array(
+            'name' => 'Book Tags',
+            'singular_name' => 'Book Tag',
+            'search_items' => 'Search Tags',
+            'all_items' => 'All Tags',
+            'edit_item' => 'Edit Tag',
+            'update_item' => 'Update Tag',
+            'add_new_item' => 'Add New Tag',
+            'new_item_name' => 'New Tag Name',
+            'menu_name' => 'Tags',
+        ),
+        'hierarchical' => false,
+        'public' => true,
+    );
+    register_taxonomy('book_tag', 'book', $args);
+}
+add_action('init', 'wp_book_register_taxonomies');
